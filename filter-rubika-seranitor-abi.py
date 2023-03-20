314438482c0f5edf13e04c336027840b3ba38e06
fakseranitor <128413819+fakseranitor@users.noreply.github.com>
2023-03-20 22:03:43 +0330

    Create filter-rubika-seranitor-abi.py

---
+++ filter-rubika-seranitor-abi.py
@@ -0,0 +1,66 @@
+from time import sleep
+import turtle
+import pyfiglet
+import time
+import os
+import sys
+
+
+
+d = '\033[31m' 
+green = '\033[32m' 
+blue = '\033[36m' 
+pink = '\033[35m' 
+yellow = '\033[93m' 
+darkblue = '\033[34m' 
+white = '\033[00m'
+print('\033[31m'+pyfiglet.figlet_format("sopi seranitor",font='slant')+ "\n "+'\033[34m'+"_"*67+white)
+
+print ('ABDIN - KURD ~ ID Rubika : @maker_kvrd')
+
+print ('sopi seranitor my id : @sopidash')
+
+print ()
+
+
+i = "code filteri rubika"
+
+print (i)
+
+os.system("clear")
+print ('seranitor')
+print ("\n[10%]\n")
+sleep(0.5)
+print ("[20%]\n")
+sleep(0.5)
+print ("[30%]\n")
+sleep(0.5)
+print ("[40%]\n")
+sleep(0.5)
+print ("[50%]\n")
+sleep(0.5)
+print ("[60%]\n")
+sleep(0.5)
+print ("[70%]\n")
+sleep(0.5)
+print ("[80%]\n")
+sleep(0.5)
+print ("[90%]\n")
+sleep(5.0)
+print ("[100%]\n")
+
+
+
+kop = """ 
+                      abdin - seranitor         
+"""
+
+print (kop)
+
+
+sleep(2.0)
+print ("code fil : yftt15k")
+
+
+
+
